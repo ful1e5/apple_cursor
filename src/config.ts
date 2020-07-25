00edd9e63d21bb8d4bddbad7a696e43d9cecc111
KaizIqbal <24286590+KaizIqbal@users.noreply.github.com>
2020-07-25 17:48:57 +0530

    📸 Animated cursors list

--- src/config.ts
+++ src/config.ts
@@ -4,12 +4,15 @@ import fs from "fs";
 // Source Directory
 const svgsDir = path.resolve(__dirname, "svg");
 
+// .svg
 let svgs = fs
   .readdirSync(svgsDir)
   .filter((file) => path.extname(file) === ".svg");
 svgs = svgs.map((svg: string) => path.resolve(svgsDir, svg));
 
+const animatedCursor = ["watch", "left_ptr_watch"];
+
 // Out Directory
 const bitmapsDir = path.resolve(process.cwd(), "bitmaps");
 
-export { svgs, bitmapsDir };
+export { svgs, animatedCursor, bitmapsDir };
