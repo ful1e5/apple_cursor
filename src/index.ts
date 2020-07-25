0771cf6777a60b088bd5ec2baa93ffbb0453552f
KaizIqbal <24286590+KaizIqbal@users.noreply.github.com>
2020-07-25 18:33:46 +0530

    🎨 Code refactor

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,21 @@
 import fs from "fs";
 
 import { generateRenderTemplate } from "./helpers/htmlTemplate";
-import { svgs } from "./config";
+import { staticSvgs } from "./config";
+
+const generateStaticSvgBitmaps = (svg: string) => {
+  fs.readFile(svg, "utf8", (error, data) => {
+    if (error) {
+      return console.log(error);
+    }
+    // Generating HTML Template
+    generateRenderTemplate(data);
+  });
+};
 
 (async () => {
   // iterate over all .svg files
-  svgs.forEach((svg) => {
-    // reading file
-    fs.readFile(svg, "utf8", (error, data) => {
-      if (error) {
-        return console.log(error);
-      }
-
-      // Generating HTML Template
-      generateRenderTemplate(data);
-    });
+  staticSvgs.forEach((svg) => {
+    generateStaticSvgBitmaps(svg);
   });
 })();
