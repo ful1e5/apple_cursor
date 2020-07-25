fa8de2813f1abfbab5e6688d07d286b188f0a275
KaizIqbal <24286590+KaizIqbal@users.noreply.github.com>
2020-07-25 17:10:02 +0530

    🖼 svgs list

--- src/config.ts
+++ src/config.ts
@@ -3,7 +3,9 @@ import fs from "fs";
 
 // Source Directory
 const svgsDir = path.resolve(__dirname, "svg");
-const svgs = fs.readdirSync(svgsDir);
+const svgs = fs
+  .readdirSync(svgsDir)
+  .filter((file) => path.extname(file) === ".svg");
 
 // Out Directory
 const bitmapsDir = path.resolve(process.cwd(), "bitmaps");
