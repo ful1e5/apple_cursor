bb35cdaa83bcb690aee47fb57d2f8749bc227ce7
KaizIqbal <24286590+KaizIqbal@users.noreply.github.com>
2020-07-25 18:16:10 +0530

    🔧 Source .svg from json data

--- src/config.ts
+++ src/config.ts
@@ -1,18 +1,13 @@
 import path from "path";
-import fs from "fs";
+import { cursors } from "./cursors.json";
 
 // Source Directory
 const svgsDir = path.resolve(__dirname, "svg");
 
-// .svg
-let svgs = fs
-  .readdirSync(svgsDir)
-  .filter((file) => path.extname(file) === ".svg");
-svgs = svgs.map((svg: string) => path.resolve(svgsDir, svg));
-
-const animatedCursor = ["watch", "left_ptr_watch"];
+// Resolve Paths for svg
+const svgs = cursors.map((svg: string) => path.resolve(svgsDir, svg));
 
 // Out Directory
 const bitmapsDir = path.resolve(process.cwd(), "bitmaps");
 
-export { svgs, animatedCursor, bitmapsDir };
+export { svgs, bitmapsDir };
