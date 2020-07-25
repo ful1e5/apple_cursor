57c424e6683ed66c60e39339c21ff83f8dbc07f0
KaizIqbal <24286590+KaizIqbal@users.noreply.github.com>
2020-07-25 18:33:14 +0530

    🔧 Configs updated

--- src/config.ts
+++ src/config.ts
@@ -1,13 +1,15 @@
 import path from "path";
-import { cursors } from "./cursors.json";
+import { staticCursors } from "./cursors.json";
 
 // Source Directory
 const svgsDir = path.resolve(__dirname, "svg");
 
 // Resolve Paths for svg
-const svgs = cursors.map((svg: string) => path.resolve(svgsDir, svg));
+const staticSvgs = staticCursors.map((svg: string) =>
+  path.resolve(svgsDir, svg)
+);
 
 // Out Directory
 const bitmapsDir = path.resolve(process.cwd(), "bitmaps");
 
-export { svgs, bitmapsDir };
+export { staticSvgs, bitmapsDir };
