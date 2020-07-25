3919925cfa1279db5f11acc9b4a0067eddfaa7ae
KaizIqbal <24286590+KaizIqbal@users.noreply.github.com>
2020-07-25 17:10:30 +0530

    🧹 Cleanup

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,3 @@
-import path from "path";
-import fs from "fs";
-import puppeteer from "puppeteer";
+import { bitmapsDir, svgs } from "./config";
 
-import { bitmapsPath } from "./config";
-
-(async () => {
-  const browser = await puppeteer.launch({
-    ignoreDefaultArgs: process.env.IS_LOCAL ? [" --single-process "] : [],
-    headless: true,
-  });
-  const page = await browser.newPage();
-  await page.goto("https://example.com");
-
-  if (!fs.existsSync(bitmapsPath)) fs.mkdirSync(bitmapsPath);
-
-  await page.pdf({ path: path.resolve(bitmapsPath, "google.pdf") });
-
-  await browser.close();
-})();
+console.log(bitmapsDir, svgs);
