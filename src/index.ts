4c1e73308cc6943231842e6fd8e6764e2974c6aa
KaizIqbal <24286590+KaizIqbal@users.noreply.github.com>
2020-07-26 17:40:24 +0530

    📸 itterate over static cursors

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,23 @@
 import fs from "fs";
+import path from "path";
 
 import { generateRenderTemplate } from "./helpers/htmlTemplate";
-import { staticSvgs } from "./config";
+// import { renderSvg } from "./helpers/render";
+import { staticSvgs, bitmapsDir } from "./config";
 
 const generateStaticSvgBitmaps = (svg: string) => {
   fs.readFile(svg, "utf8", (error, data) => {
     if (error) {
       return console.log(error);
     }
+
     // Generating HTML Template
     generateRenderTemplate(data);
+
+    // rendering svg
+    let bitmap = path.parse(svg).base;
+    bitmap = path.resolve(bitmapsDir, bitmap);
+    console.log(bitmap);
   });
 };
 
