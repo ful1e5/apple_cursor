cce5a2ed842ddb55a96be5cadf3fe66e45eca90f
KaizIqbal <24286590+KaizIqbal@users.noreply.github.com>
2020-07-26 18:14:08 +0530

    ⏸ Await method

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import { generateRenderTemplate } from "./helpers/htmlTemplate";
 // import { renderSvg } from "./helpers/render";
 import { staticSvgs, bitmapsDir } from "./config";
 
-const generateStaticSvgBitmaps = (svg: string) => {
+const generateStaticSvgBitmaps = async (svg: string) => {
   fs.readFile(svg, "utf8", (error, data) => {
     if (error) {
       return console.log(error);
@@ -14,16 +14,15 @@ const generateStaticSvgBitmaps = (svg: string) => {
     // Generating HTML Template
     generateRenderTemplate(data);
 
-    // rendering svg
+    // preparing paths
     let bitmap = path.parse(svg).base;
     bitmap = path.resolve(bitmapsDir, bitmap);
+
     console.log(bitmap);
   });
 };
 
-(async () => {
-  // iterate over all .svg files
-  staticSvgs.forEach((svg) => {
-    generateStaticSvgBitmaps(svg);
-  });
-})();
+// iterate over satic .svg files
+staticSvgs.forEach(async (svg) => {
+  await generateStaticSvgBitmaps(svg);
+});
