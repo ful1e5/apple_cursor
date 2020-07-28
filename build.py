094bacd2be948fb687acdb993ce6fc3f7117782f
KaizIqbal <24286590+KaizIqbal@users.noreply.github.com>
2020-07-28 17:02:47 +0530

    🗂 Out Directory structure

--- build.py
+++ build.py
@@ -10,3 +10,8 @@ with open('./hotspots.json', 'r') as hotspot_file:
     config = json.loads(hotspot_file.read())
     build_cursor_theme(name="macOS Big Sur", image_dir="./bitmaps",
                        cursor_sizes=sizes, out_path="out", archive=False, delay=30)
+
+# Rename directory & cleanup
+shutil.move("./out/macOS Big Sur/x11", "./out/macOSBigSur")
+shutil.move("./out/macOS Big Sur/win", "./out/macOSBigSur_Windows")
+shutil.rmtree("./out/macOS Big Sur")
