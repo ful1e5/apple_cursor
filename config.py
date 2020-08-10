e7d509295b69fbe43cdc3ea3000c493dcee47824
ful1e5 <24286590+KaizIqbal@users.noreply.github.com>
2020-08-10 11:25:59 +0530

    🔧 Windows cursors config

--- config.py
+++ config.py
@@ -19,6 +19,24 @@ with open("./package.json") as f:
     author = data["author"]
 
 # Windows Cursors Config
+windows_cursors = {
+    "sb_up_arrow.cur": "alt-select.cur",
+    "bd_double_arrow.cur": "diagonal-resize-1.cur",
+    "bottom_left_corner.cur": "diagonal-resize-2.cur",
+    "pencil.cur": "handwriting.cur",
+    "dnd-ask.cur": "help-select.cur",
+    "right_side.cur": "horizontal-resize.cur",
+    "hand2.cur": "link-select.cur",
+    "all-scroll.cur": "move.cur",
+    "left_ptr.cur": "normal-select.cur",
+    "X_cursor.cur": "pirate.cur",
+    "crosshair.cur": "precision-select.cur",
+    "xterm.cur": "text-select.cur",
+    "circle.cur": "unavailable.cur",
+    "bottom_side.cur": "vertical-resize.cur",
+    "wait.ani": "busy.ani",
+    "left_ptr_watch.ani": "working-in-background.ani"
+}
 
 # Windows install.inf file content
 with open("./scripts/windows.inf") as f:
