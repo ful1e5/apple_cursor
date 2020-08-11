7409f054de757fa7b068ff7285cdcbe5e289dca3
ful1e5 <24286590+KaizIqbal@users.noreply.github.com>
2020-08-11 09:01:30 +0530

    ⬆ Update Package version

--- scripts/install.sh
+++ scripts/install.sh
@@ -5,7 +5,7 @@ INPUT=$1
 
 ROOT_UID=0
 DEST_DIR=
-URL="https://github.com/ful1e5/apple_cursor/releases/download/1.0.1-beta/macOSBigSur.tar"
+URL="https://github.com/ful1e5/apple_cursor/releases/download/1.0.2/macOSBigSur.tar"
 macOSBigSur="./macOSBigSur"
 DOWNLOAD_FILE_NAME="$macOSBigSur.tar"
 
