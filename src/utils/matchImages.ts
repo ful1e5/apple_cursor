f0ca4c56898690f23933525b8197fa8a13857edb
ful1e5 <24286590+ful1e5@users.noreply.github.com>
2020-08-29 17:32:25 +0530

    🧹 Diff files cleanup

--- src/utils/matchImages.ts
+++ src/utils/matchImages.ts
@@ -1,27 +1,19 @@
-import fs from "fs";
-import path from "path";
 import { PNG } from "pngjs";
 import pixelmatch from "pixelmatch";
 
-export const matchImages = (img1Buff: Buffer, img2Buff: Buffer) => {
+interface MatchImagesArgs {
+  img1Buff: Buffer;
+  img2Buff: Buffer;
+}
+
+export const matchImages = ({ img1Buff, img2Buff }: MatchImagesArgs) => {
   const img1 = PNG.sync.read(img1Buff);
   const img2 = PNG.sync.read(img2Buff);
   const { width, height } = img1;
 
   const diff = new PNG({ width, height });
 
-  const out = pixelmatch(img1.data, img2.data, diff.data, width, height, {
-    threshold: 0.3,
+  return pixelmatch(img1.data, img2.data, diff.data, width, height, {
+    threshold: 0.25,
   });
-
-  if (process.env.NODE_ENV === "development") {
-    const diffFilesPath = path.resolve(process.cwd(), "diff");
-    if (!fs.existsSync(diffFilesPath)) fs.mkdirSync(diffFilesPath);
-    fs.writeFileSync(
-      path.resolve(diffFilesPath, `${out}.png`),
-      PNG.sync.write(diff)
-    );
-  }
-
-  return out;
 };
