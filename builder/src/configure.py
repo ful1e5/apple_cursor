e1d06dfb85d310a97ef7ce12eb474ac4695cdb8c
ful1e5 <24286590+ful1e5@users.noreply.github.com>
2021-11-06 17:24:31 +0530

    format & yarn upgrade

--- builder/src/configure.py
+++ builder/src/configure.py
@@ -4,6 +4,7 @@
 from typing import Any, Dict, Tuple, TypeVar
 
 from clickgen.util import PNGProvider
+
 from .constants import WIN_CURSORS_CFG, WIN_DELAY, X_CURSORS_CFG, X_DELAY
 
 X = TypeVar("X")
